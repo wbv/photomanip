c7b4d3d0ccc19959aca66911381a392ab550419e
Walter B. Vaughan <wbv@users.noreply.github.com>
2022-02-06 17:13:31 -0500

    commit parsing atrocities :(

--- src/image.rs
+++ src/image.rs
@@ -40,26 +40,122 @@ pub fn load_from_file(path: &str) -> io::Result<Image> {
     let mut file = File::open(path)?;
     let n = file.read_to_end(&mut data)?;
 
+    // interpret the kind of PPM from the magic sequence
     let (grayscale, ascii) = 
         match std::str::from_utf8(&data[0..2]) {
-           Ok("P6") => { println!("Found a P6"); (false, false) },
-           Ok("P5") => { println!("Found a P5"); (true,  false) },
-           Ok("P3") => { println!("Found a P3"); (false, true)  },
-           Ok("P2") => { println!("Found a P2"); (true,  true)  },
+           Ok("P6") => { (false, false) },
+           Ok("P5") => { (true,  false) },
+           Ok("P3") => { (false, true)  },
+           Ok("P2") => { (true,  true)  },
            _ => {
                eprintln!("Not a valid PPM/PGM file: '{}'", path);
                return Err(io::Error::new(
-                io::ErrorKind::InvalidData, "Not a PPM/PGM file"
+                   io::ErrorKind::InvalidData, "Not a PPM/PGM file"
                ))
            }
         };
 
+    // scan through the file header following the magic seqence, byte at a time
+    // `scanner` is our index into data as we scan
+    let mut scanner: usize = 2;
+
+    // skip whitespace following the magic sequence
+    for i in scanner..data.len() {
+        if data[i].is_ascii_whitespace() {
+            scanner = i+1;
+            break;
+        }
+    }
+
+    // check for line comment, read 'til EOL
+    if data[scanner] == b'#' {
+        while data[scanner] != b'\n' {
+            scanner = scanner + 1;
+        }
+    }
+
+    // skip until width string
+    for i in scanner..data.len() {
+        if data[i].is_ascii_whitespace() {
+            scanner = i+1;
+            break;
+        }
+    }
+
+    // form the width from the next non-whitespace characters
+    let mut width_str = String::new();
+    for i in scanner..data.len() {
+        if !data[i].is_ascii_whitespace() {
+            width_str.push(data[i].clone() as char)
+        } else {
+            break;
+        }
+    }
+    let width = match width_str.parse::<usize>() {
+        Ok(val) => val,
+        Err(_) => {
+            eprintln!("Invalid ascii width found: '{}'", width_str);
+            eprintln!("at postion {}", scanner);
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData, "Invalid width parameter"
+            ))
+        }
+    };
+
+    // skip until width string
+    for i in scanner..data.len() {
+        if data[i].is_ascii_whitespace() {
+            scanner = i+1;
+            break;
+        }
+    }
+
+    // form the height from the next non-whitespace characters
+    let mut height_str = String::new();
+    for i in scanner..data.len() {
+        if !data[i].is_ascii_whitespace() {
+            height_str.push(data[i].clone() as char)
+        } else {
+            break;
+        }
+    }
+    let height = match height_str.parse::<usize>() {
+        Ok(val) => val,
+        Err(_) => {
+            eprintln!("Invalid ascii height found: '{}'", height_str);
+            eprintln!("at postion {}", scanner);
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData, "Invalid height parameter"
+            ))
+        }
+    };
+
+    // form the maxval from the next non-whitespace characters
+    let mut maxval_str = String::new();
+    for i in scanner..data.len() {
+        if !data[i].is_ascii_whitespace() {
+            maxval_str.push(data[i].clone() as char)
+        } else {
+            break;
+        }
+    }
+    let maxval = match maxval_str.parse::<usize>() {
+        Ok(val) => val,
+        Err(_) => {
+            eprintln!("Invalid ascii maxval found: '{}'", maxval_str);
+            eprintln!("at postion {}", scanner);
+            return Err(io::Error::new(
+                io::ErrorKind::InvalidData, "Invalid height parameter"
+            ))
+        }
+    };
+
     if grayscale {
         Ok(Image::GrayImage(Box::new(
             GrayImage {
-                width: 1,
-                height: 1,
-                maxval: 1,
+                width: width,
+                height: height,
+                maxval: maxval,
                 pixels: vec![0],
             }
         )))
@@ -68,9 +164,9 @@ pub fn load_from_file(path: &str) -> io::Result<Image> {
     else {
         Ok(Image::ColorImage(Box::new(
             ColorImage {
-                width: 1,
-                height: 1,
-                maxval: 1,
+                width: width,
+                height: height,
+                maxval: maxval,
                 rpixels: vec![0],
                 gpixels: vec![0],
                 bpixels: vec![0]
@@ -272,14 +368,32 @@ mod tests {
     }
 
     #[test]
-    fn open() {
-        let bad_img = load_from_file("bogus");
-        assert_eq!(bad_img.is_err(), true);
+    fn open_nonexistent_file() {
+        let no_file = load_from_file("bogus");
+        assert!(no_file.is_err());
+    }
 
-        let img_path = img_folder() + "color_ascii_baldy.ppm";
-        println!("{}", img_path);
-        let good_img = load_from_file(&img_path);
-        assert_eq!(good_img.is_ok(), true);
+    #[test]
+    fn open_valid_image_files() {
+        // test all 4 variants of 
+        let test_images = vec![
+            "color_ascii_baldy.ppm",
+            "gray_ascii_baldy.pgm",
+            "color_raw_baldy.ppm",
+            "gray_raw_baldy.pgm",
+        ];
+        for i in &test_images {
+            let img_path = img_folder() + i;
+            let good_img = load_from_file(&img_path);
+            assert!(good_img.is_ok());
+        }
+    }
+
+    #[test]
+    fn open_non_image_file() {
+        let img_path = img_folder() + "../Cargo.toml";
+        let not_image = load_from_file(&img_path);
+        assert!(not_image.is_err());
     }
 
 }
